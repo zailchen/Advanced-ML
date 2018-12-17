bfc03e189e386592fc267b1cf1ff89b72625a40d
Ziyu Chen <zc2393@columbia.edu>
2018-12-17 13:04:03 -0500

    Update ae_utils.py

--- final/ae_utils.py
+++ final/ae_utils.py
@@ -2,19 +2,14 @@
 import os
 import re
 import sys
-import h5py
 import time
 import random
 import string
-import contextlib
 import multiprocessing
 import pandas as pd
 import numpy as np
 import tensorflow as tf
 from model import ae
-
-identifier = '(([a-zA-Z]_)?([a-zA-Z0-9_]*))'
-replacement_field = '{' + identifier + '}'
 
 
 def reset():
@@ -22,64 +17,8 @@ def reset():
     random.seed(19)
     np.random.seed(19)
     tf.set_random_seed(19)
-
-
-def load_phenotypes(pheno_path):
-    pheno = pd.read_csv(pheno_path)
-    pheno = pheno[pheno['FILE_ID'] != 'no_filename']
-
-    pheno['DX_GROUP'] = pheno['DX_GROUP'].apply(lambda v: int(v)-1)
-    pheno['SITE_ID'] = pheno['SITE_ID'].apply(lambda v: re.sub('_[0-9]', '', v))
-    pheno['SEX'] = pheno['SEX'].apply(lambda v: {1: "M", 2: "F"}[v])
-    pheno['MEAN_FD'] = pheno['func_mean_fd']
-    pheno['SUB_IN_SMP'] = pheno['SUB_IN_SMP'].apply(lambda v: v == 1)
-    pheno["STRAT"] = pheno[["SITE_ID", "DX_GROUP"]].apply(lambda x: "_".join([str(s) for s in x]), axis=1)
-
-    pheno.index = pheno['FILE_ID']
-
-    return pheno[['FILE_ID', 'DX_GROUP', 'SEX', 'SITE_ID', 'MEAN_FD', 'SUB_IN_SMP', 'STRAT']]
-
-
-def hdf5_handler(filename, mode="r"):
-    h5py.File(filename, "a").close()
-    propfaid = h5py.h5p.create(h5py.h5p.FILE_ACCESS)
-    settings = list(propfaid.get_cache())
-    settings[1] = 0
-    settings[2] = 0
-    propfaid.set_cache(*settings)
-    with contextlib.closing(h5py.h5f.open(filename, fapl=propfaid)) as fid:
-        return h5py.File(fid, mode)
-
-
-def load_fold(patients, experiment, fold):
-
-    derivative = experiment.attrs["derivative"]
-
-    X_train = []
-    y_train = []
-    for pid in experiment[fold]["train"]:
-        X_train.append(np.array(patients[pid][derivative]))
-        y_train.append(patients[pid].attrs["y"])
-
-    X_valid = []
-    y_valid = []
-    for pid in experiment[fold]["valid"]:
-        X_valid.append(np.array(patients[pid][derivative]))
-        y_valid.append(patients[pid].attrs["y"])
-
-    X_test = []
-    y_test = []
-    for pid in experiment[fold]["test"]:
-        X_test.append(np.array(patients[pid][derivative]))
-        y_test.append(patients[pid].attrs["y"])
-
-    return np.array(X_train), y_train, \
-           np.array(X_valid), y_valid, \
-           np.array(X_test), y_test
-
-
+    
 class SafeFormat(dict):
-
     def __missing__(self, key):
         return "{" + key + "}"
 
@@ -87,70 +26,16 @@ class SafeFormat(dict):
         if key not in self:
             return self.__missing__(key)
         return dict.__getitem__(self, key)
-
-
+    
 def merge_dicts(*dict_args):
     result = {}
     for dictionary in dict_args:
         result.update(dictionary)
     return result
-
-
+    
 def format_config(s, *d):
-    dd = merge_dicts(*d)
-    return string.Formatter().vformat(s, [], SafeFormat(dd))
-
-
-def elapsed_time(tstart):
-    tnow = time.time()
-    total = tnow - tstart
-    m, s = divmod(total, 60)
-    h, m = divmod(m, 60)
-    return "%d:%02d:%02d" % (h, m, s)
-
-
-def run_progress(callable_func, items, message=None, jobs=1):
-
-    results = []
-
-    print('Starting pool of %d jobs' % jobs)
-
-    current = 0
-    total = len(items)
-
-    if jobs == 1:
-        results = []
-        for item in items:
-            results.append(callable_func(item))
-            current = len(results)
-            if message is not None:
-                args = {'current': current, 'total': total}
-                sys.stdout.write("\r" + message.format(**args))
-                sys.stdout.flush()
-
-    # Or allocate a pool for multithreading
-    else:
-        pool = multiprocessing.Pool(processes=jobs)
-        for item in items:
-            pool.apply_async(callable_func, args=(item,), callback=results.append)
-
-        while current < total:
-            current = len(results)
-            if message is not None:
-                args = {'current': current, 'total': total}
-                sys.stdout.write("\r" + message.format(**args))
-                sys.stdout.flush()
-            time.sleep(0.5)
-
-        pool.close()
-        pool.join()
-
-    print()
-    return results
-
-
-def root():
-    return os.path.dirname(os.path.realpath(__file__))
+    merge_dic = merge_dicts(*d)
+    return string.Formatter().vformat(s, [], SafeFormat(merge_dic))
 
 
 def to_softmax(n_classes, classe):
